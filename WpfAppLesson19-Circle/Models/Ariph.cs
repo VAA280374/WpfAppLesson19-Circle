dc503e906774a65545d8bef5350be5a55e8194f8
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2022-07-17 14:33:19 +0300

    Добавьте файлы проекта.

---
+++ WpfAppLesson19-Circle/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppLesson19_Circle.Models
+{
+    static class Ariph
+    {
+        public static double Calc(double a) => 2 * a * Math.PI;
+    }
+}
---
+++ WpfAppLesson19-Circle/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfAppLesson19_Circle.Models;
+
+namespace WpfAppLesson19_Circle.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            { 
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            { 
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CalcCommand { get; }
+
+        private void OnCalcCommandExecute(object p)
+        {
+            Number2 = Ariph.Calc(Number1);
+        }
+
+        private bool CanCalcCommandExecuted (object p)
+        {
+            if (Number1 > 0) return true;
+            else return false;
+        }
+
+        public MainWindowViewModel()
+        { CalcCommand = new RelayCommand(OnCalcCommandExecute, CanCalcCommandExecuted); }
+
+    }
+}
+
